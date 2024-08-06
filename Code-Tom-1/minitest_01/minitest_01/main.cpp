c3e1c6cbe94b5994cce992d8f2ffa8c369623211
BDOTimer <41667659+BDOTimer@users.noreply.github.com>
2024-08-07 01:07:34 +0300

    +

--- Code-Tom-1/minitest_01/minitest_01/main.cpp
+++ Code-Tom-1/minitest_01/minitest_01/main.cpp
@@ -2,28 +2,33 @@
 
 #define l(v) std::wcout << #v << " = " << (v) << "\n";
 
-struct  Test
-{       Test(){}
-       ~Test(){ delete p; }
+struct  Vec2{ int x, y; };
 
-    int n{0};
+Vec2 U = {120, 120};     /// Верхняя левая точка координаты шахматной доски.
+Vec2 D = {200, 200};     /// Нижняя правая точка координаты шахматной доски.
 
-    static inline Test* p = nullptr;
+void check(const Vec2 M) /// Рандомный клик мауса с координатами { M.x, M.y }
+{
+    static std::string bx = {"abcdefgh"};
+    static std::string by = {"12345678"};
 
-    static void init()
-    {   if(p == nullptr) p = new Test;
-        else return;
-        p->n  = 2024;
+    const int Cell = (D.x - U.x) / 8;
+
+    Vec2 B = {M.x - U.x,
+              M.y - U.y};
+
+    unsigned ix = B.x /  Cell;
+    unsigned iy = B.y /  Cell;
+
+    if(ix < 8 && iy < 8)
+    {   std::cout << "Cell::" << bx[ix] << "," << by[iy] << '\n';
     }
-};
-
-Test test;
+    else std::cout << "Past the board\n";
+}
 
 int main()
 {
-    Test::init();
-
-    std::cout << "Hello Start ..." << std::endl;
-    std::cout << Test::p->n        << std::endl;
-    std::cout << "Finished."       << std::endl;
+    check({121, 121});
+    check({199, 199});
+    check({199, 222});
 }
